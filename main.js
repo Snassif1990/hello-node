69550f95016fc81063b976eb8f7199725b11c783
Sam Nassif <98352078+Snassif1990@users.noreply.github.com>
2022-02-04 23:52:00 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+console.log("Hello World");
+console.log("\n");
+console.log("Goodbye Cruel World!");
+
+
+let person = "Guy Fieri";
+let person2 = "Toddrick Merlin";
+
+let people = person + " and " + person2;
+
+console.log(people);
+
+let x = 5;
+let y = 7;
+
+let sum = x+y;
+
+console.log("The sum of x an y is: " + sum);
+
+console.log("The sum of " + 5 + " and " + 7 + " is " + sum);
+console.log("The sum of " + x + " and " + y + " is " + sum); 
+
+console.log("\n")
+
+let a = "HH   HH        lll lll                                  lll      dd !!! ";
+let b = "HH   HH   eee  lll lll  oooo   ww      ww  oooo  rr rr  lll      dd !!! ";
+let c = "HHHHHHH ee   e lll lll oo  oo  ww      ww oo  oo rrr  r lll  dddddd !!! ";
+let d = "HH   HH eeeee  lll lll oo  oo   ww ww ww  oo  oo rr     lll dd   dd     ";
+let e = "HH   HH  eeeee lll lll  oooo     ww  ww    oooo  rr     lll  dddddd !!! ";
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+
+console.log("\n");
+
+let f = "TTTTTT H  H EEEE  EEEE N   N DDD  ";
+let g = "  TT   H  H E     E    NN  N D  D ";
+let h = "  TT   HHHH EEE   EEE  N N N D  D ";
+let i = "  TT   H  H E     E    N  NN D  D ";
+let j = "  TT   H  H EEEE  EEEE N   N DDD  ";
+
+console.log(f);
+console.log(g);
+console.log(h);
+console.log(i);
+console.log(j);
+
+console.log("\n")
